1f50b4a5ec40f2da693dc1d397003a00cd8af22f
Karolina Opitek <kjopi193@learner.dvusd.org>
2024-01-08 09:29:54 -0700

    Monday. 2 Update

---
+++ convertor/convertor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convertor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your number");
+            int num = 0;
+            int.TryParse(Console.ReadLine(),out num);
+            Console.WriteLine(num *2);
+            Console.ReadLine();
+        }
+    }
+}
